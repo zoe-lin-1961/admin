a28b8cb107f85e7c1ed42cd05dbe7eee1b6a7c02
zoe-lin-1961 <q1961403417@gmail.com>
2021-11-12 13:29:59 +0800

    33222

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/eny sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A 
+git commit -m 'New Deployment'
+git push -f git@github.com:zoe-lin-1961/admin.git master:gh-pages
+
+cd -
